5dbf6a4910c905c08ec312e00e67c638bd858802
attilathedud <attilathedud@gmail.com>
2021-09-11 17:25:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+    An example of a modified Windows debugger that will attach to a running Wesnoth process, locate all call instructions and change them to an int3 instruction.
+    When the breakpoint is hit, the location will be logged and the instruction will be restored. Then, after the instruction is executed, an int3 instruction will
+    be rewritten to the location.
+    
+    The code and approach are discussed in the article at: https://gamehacking.academy/lesson/42
+*/
+
 #include <windows.h>
 #include <tlhelp32.h>
 #include <stdio.h>
